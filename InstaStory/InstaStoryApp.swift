040a20b65963134272dd4770f8d64f052334f2d4
Samuel Neveu <samuel.neveugall@gmail.com>
2025-06-09 14:17:10 +0200

    Initial Commit

---
+++ InstaStory/InstaStoryApp.swift
@@ -0,0 +1,32 @@
+//
+//  InstaStoryApp.swift
+//  InstaStory
+//
+//  Created by Samuel Neveu on 09/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InstaStoryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
